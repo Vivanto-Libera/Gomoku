f77963116a4f371e0f5892485a360537c96dc32b
Vivanto <1425078256@qq.com>
2025-08-30 11:59:10 +0800

    Add a button to tile, and changed Page.png

--- Tile.cs
+++ Tile.cs
@@ -3,31 +3,31 @@ using System;
 
 public partial class Tile : TextureRect
 {
-    public enum State
-    {
-        None,
-        O,
-        X,
-    }
-    private State state;
-    public void SetState(State newState) 
-    {
-        state = newState;
-        switch (newState) 
-        {
-            case State.None:
-                Texture = null;
-                break;
-            case State.O:
-                Texture = GD.Load<Texture2D>("O.png");
-                break;
-            case State.X:
-                Texture = GD.Load<Texture2D>("X.png");
-                break;
-        }
-    }
-    public State GetState()
-    {
-        return state;
-    }
+	public enum State
+	{
+		None,
+		O,
+		X,
+	}
+	private State state;
+	public void SetState(State newState) 
+	{
+		state = newState;
+		switch (newState) 
+		{
+			case State.None:
+				Texture = null;
+				break;
+			case State.O:
+				Texture = GD.Load<Texture2D>("O.png");
+				break;
+			case State.X:
+				Texture = GD.Load<Texture2D>("X.png");
+				break;
+		}
+	}
+	public State GetState()
+	{
+		return state;
+	}
 }
