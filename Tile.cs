23cb37e96bcdf776b38fd8381d20f7f318ca914c
Vivanto <1425078256@qq.com>
2025-08-30 11:50:23 +0800

    Wrote tile class

---
+++ Tile.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Tile : TextureRect
+{
+    public enum State
+    {
+        None,
+        O,
+        X,
+    }
+    private State state;
+    public void SetState(State newState) 
+    {
+        state = newState;
+        switch (newState) 
+        {
+            case State.None:
+                Texture = null;
+                break;
+            case State.O:
+                Texture = GD.Load<Texture2D>("O.png");
+                break;
+            case State.X:
+                Texture = GD.Load<Texture2D>("X.png");
+                break;
+        }
+    }
+    public State GetState()
+    {
+        return state;
+    }
+}
